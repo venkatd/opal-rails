4797f6b75f528a1aff22333136833f9708df4498
Elia Schito <elia@schito.me>
2012-07-14 22:45:21 +0200

    fixup! Move rails

--- spec/support/rails.rb
+++ spec/support/rails.rb
@@ -1,5 +1,5 @@
 # Rails
 ENV['RAILS_ENV'] = 'test'
-require File.expand_path('../../test_app/config/environment.rb',  __FILE__)
+require File.expand_path('../../../test_app/config/environment.rb',  __FILE__)
 require 'rspec/rails'
 Rails.backtrace_cleaner.remove_silencers!

3cea02b2d62a1d7ae1d7d08a1a79018122d11443
Elia Schito <elia@schito.me>
2012-07-14 22:35:03 +0200

    Move rails spec stuff to a separate file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,6 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'opal-rails'
 
-
-# Configure Rails Environment
-ENV['RAILS_ENV'] = 'test'
-
-require File.expand_path('../../test_app/config/environment.rb',  __FILE__)
-require 'rspec/rails'
-
-Rails.backtrace_cleaner.remove_silencers!
-
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
---
+++ spec/support/rails.rb
@@ -0,0 +1,5 @@
+# Rails
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../test_app/config/environment.rb',  __FILE__)
+require 'rspec/rails'
+Rails.backtrace_cleaner.remove_silencers!
